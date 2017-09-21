df9e5e9deb1b81e3f0729ea59ea23b8dbe910ab5
bjones325 <acer_mortem@yahoo.com>
2017-09-21 15:48:28 -0400

    Created Project Commit

---
+++ app/src/main/java/edu/gatech/cats/cats_2340/OpeningActivity.java
@@ -0,0 +1,13 @@
+package edu.gatech.cats.cats_2340;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OpeningActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_opening);
+    }
+}
