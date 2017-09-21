660336b0f52bfac40be1d6ffca46b71c4d12219a
trexmix <elijahtrexler@gmail.com>
2017-09-21 19:40:36 -0400

    Added a rudimentary model with a hard coded username and password, fixed some errors

--- app/src/main/java/edu/gatech/cats/cats_2340/model/Model.java
+++ app/src/main/java/edu/gatech/cats/cats_2340/model/Model.java
@@ -2,7 +2,29 @@ package edu.gatech.cats.cats_2340.model;
 
 /**
  * Created by acer_ on 9/21/2017.
+ * Modified 9/21/17
+ *
+ * Basic model class
  */
 
 public class Model {
+    private static final Model _instance = new Model();
+
+    private String user;
+    private String pass;
+
+    private boolean loggedIn = false;
+
+    public static Model getInstance() {
+        return _instance;
+    }
+
+    private Model() {
+        user = "user";
+        pass = "pass";
+    }
+
+    public boolean attemptLogin(String user, String pass) {
+        return (user.equals("user") && pass.equals("pass"));
+    }
 }
