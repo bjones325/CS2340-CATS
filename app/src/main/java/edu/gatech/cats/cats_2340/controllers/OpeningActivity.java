95640eae2d3ff691d8fc9812035f374b63b62140
bjones325 <acer_mortem@yahoo.com>
2017-09-21 21:47:18 -0400

    Worked on OpeningActivity

--- app/src/main/java/edu/gatech/cats/cats_2340/controllers/OpeningActivity.java
+++ app/src/main/java/edu/gatech/cats/cats_2340/controllers/OpeningActivity.java
@@ -1,7 +1,9 @@
 package edu.gatech.cats.cats_2340.controllers;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 import edu.gatech.cats.cats_2340.R;
 
@@ -11,5 +13,11 @@ public class OpeningActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_opening);
+
+        Button loginButton = (Button) findViewById(R.id.loginButton);
+        loginButton.setOnClickListener((view) -> {
+            Intent intent = new Intent(getBaseContext(), LoginActivity.class);
+            startActivity(intent);
+        });
     }
 }
