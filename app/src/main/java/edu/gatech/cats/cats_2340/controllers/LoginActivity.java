afdbf2047360f7e6cc4e08905ccd0d4eb1b8422d
bjones325 <acer_mortem@yahoo.com>
2017-09-25 15:48:05 -0400

    Updated LoginActivity

--- app/src/main/java/edu/gatech/cats/cats_2340/controllers/LoginActivity.java
+++ app/src/main/java/edu/gatech/cats/cats_2340/controllers/LoginActivity.java
@@ -15,32 +15,34 @@ import edu.gatech.cats.cats_2340.model.Model;
 //A basic log in screen. Can log in or return to splash screen from here
 public class LoginActivity extends AppCompatActivity{
 
+    private TextView failedSuccessText;
+    private TextView usernameField;
+    private TextView passwordField;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        usernameField = (TextView) findViewById(R.id.usernameField);
+        passwordField = (TextView) findViewById(R.id.passwordField);
         //Log-in failed text
-        TextView failedSuccess = (TextView) findViewById(R.id.textView4);
+
+        failedSuccessText = (TextView) findViewById(R.id.textView4);
         //We want it invisible at the start
-        failedSuccess.setVisibility(View.INVISIBLE);
+        failedSuccessText.setVisibility(View.INVISIBLE);
 
     }
 
     protected void onLoginPressed(View view) {
         Model model = Model.getInstance();
-        TextView usernameField = (TextView) findViewById(R.id.usernameField);
         String username = usernameField.getText().toString();
-
-        TextView passwordField = (TextView) findViewById(R.id.passwordField);
         String password = passwordField.getText().toString();
 
         if(model.attemptLogin(username, password)) {
             startActivity(new Intent(getBaseContext(),ApplicationActivity.class));
             finish();
         } else {
-            //@Blake why do we search for it twice? See onCreate()
-            TextView failedSuccess = (TextView) findViewById(R.id.textView4);
-            failedSuccess.setVisibility(View.VISIBLE);
+            failedSuccessText.setVisibility(View.VISIBLE);
         }
     }
 
