b25f407c8a329dcd2a39dba944b15fd643238e92
trexmix <elijahtrexler@gmail.com>
2017-09-28 16:52:12 -0400

    Pushing for Ruchi to download working register button to test UI

--- app/src/main/java/edu/gatech/cats/cats_2340/model/Model.java
+++ app/src/main/java/edu/gatech/cats/cats_2340/model/Model.java
@@ -1,6 +1,8 @@
 package edu.gatech.cats.cats_2340.model;
 
 import android.util.Log;
+import java.util.List;
+import java.util.ArrayList;
 
 /**
  * Created by acer_ on 9/21/2017.
@@ -16,6 +18,8 @@ public class Model {
     private String user;
     private String pass;
 
+    private List<User> userList = new ArrayList<User>();
+
     private boolean loggedIn = false;
 
     public static Model getInstance() {
@@ -30,8 +34,26 @@ public class Model {
     //Basic logic for a log in implemented here. The username and password are hardcoded but will
     //obviously be replaced in the future
     public boolean attemptLogin(String user, String pass) {
-        loggedIn = true;
-        return (user.equals("user") && pass.equals("pass"));
+        boolean foundUser = false;
+        for (User u : userList) {
+            if (u.getName() == user) {
+                foundUser = true;
+
+                if (u.getPass() == pass) {
+                    loggedIn = true;
+                    return true;
+                } else {
+                    Log.d("ERROR:", "Incorrect password"); //TODO: Add in user facing
+                    return false;
+                }
+            }
+        }
+
+        if (!foundUser) {
+            Log.d("ERROR:", "Username not found"); //TODO: Add in user facing
+        }
+
+        return false;
     }
 
     public void logout() {
@@ -39,5 +61,9 @@ public class Model {
             Log.d("ERROR:", "User logged out but was not logged in!");
         }
         loggedIn = false;
+    }
+
+    public void register(User u) {
+        userList.add(u);
     }
 }
