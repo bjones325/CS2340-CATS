3c90271df9c821220e9b8af2a2dfabf25a0e82ae
elena.may <elena.p.may@gmail.com>
2017-10-12 16:21:46 -0400

    Added activity_input_rat_sighting.xml

---
+++ app/src/main/java/edu/gatech/cats/cats_2340/controllers/InputRatSighting.java
@@ -0,0 +1,15 @@
+package edu.gatech.cats.cats_2340.controllers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.gatech.cats.cats_2340.R;
+
+public class InputRatSighting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input_rat_sighting);
+    }
+}
